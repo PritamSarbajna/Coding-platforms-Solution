7e4cc85114f8c6fad81727f4f104e0239502e389
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2021-12-31 01:07:08 +0530

    Create FCTRL2.cpp

---
+++ Code Chef/FCTRL2/FCTRL2.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace std;
+using namespace boost::multiprecision;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int num;
+        cin >> num;
+        cpp_int result = 1;
+
+         for (int i = 1; i <= num; ++i){
+             result *= i;
+         }
+         cout << result << endl;
+    }
+    return 0;
+}
