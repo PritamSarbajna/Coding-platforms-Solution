4a522d2a599a6fde40bcb5e2a56b5923543f9871
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2021-12-31 01:13:25 +0530

    Create FLOW008.cpp

---
+++ Code Chef/FLOW008/FLOW008.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int num;
+        cin >> num;
+
+        if(num >= 10){
+            cout << "-1" << endl;
+        }
+        else{
+            cout << "Thanks for helping Chef!" << endl;
+        }
+    }
+
+    return 0;
+}
