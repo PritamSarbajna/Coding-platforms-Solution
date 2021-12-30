bad2c651fa0f7a9f5f992a302ca14b3ad6756915
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2021-12-31 01:10:14 +0530

    Create FLOW002.cpp

---
+++ Code Chef/FLOW002/FLOW002.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void remainder(int a, int b){
+    cout << a%b << endl;
+}
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+
+        remainder(a, b);
+    }
+    return 0;
+}
+© 2021 GitHub, Inc.
