ba9aed9d50968369b8f736cfd0d193c642e1ef68
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2021-12-31 01:05:06 +0530

    Create COCONUT.cpp

---
+++ Code Chef/COCONUT/COCONUT.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int Xa, Xb;
+        int xa, xb;
+        cin >> xa >> xb >> Xa >> Xb;
+
+        cout << (Xa / xa) + (Xb / xb) << endl;
+    }
+
+    return 0;
+}
