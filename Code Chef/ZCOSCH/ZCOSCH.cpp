e283fe43af9800a135f6e5ed1ae5940fcffba66e
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2021-12-31 18:14:25 +0530

    Create ZCOSCH.cpp

---
+++ Code Chef/ZCOSCH/ZCOSCH.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int r;
+    cin >> r;
+
+    if(r>=1 && r<=50){
+        cout << 100 << endl;
+    }
+    else if (r>50 && r<=100){
+        cout << 50 << endl;
+    }
+    else{
+        cout << 0 << endl;
+    }
+        return 0;
+}
