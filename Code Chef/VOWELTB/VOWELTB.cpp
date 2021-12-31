3900d51568e4f87573655a7690bfa942ac0f80c8
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2021-12-31 14:41:04 +0530

    Create VOWELTB.cpp

---
+++ Code Chef/VOWELTB/VOWELTB.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    char C;
+    cin >> C;
+
+    if (C == 'A' || C == 'E' || C == 'I' || C == 'O' || C == 'U'){
+        cout << "Vowel" << endl;
+    }
+    else{
+        cout << "Consonant" << endl;
+    }
+
+    return 0;
+}
