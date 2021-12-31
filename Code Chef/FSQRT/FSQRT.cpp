88eddd8d9e75009ffdf991213eb8920837444e1f
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2021-12-31 14:34:09 +0530

    Create FSQRT.cpp

---
+++ Code Chef/FSQRT/FSQRT.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int num;
+        cin >> num;
+        cout << sqrt(num) << endl;
+    }
+    
+    return 0;
+}
