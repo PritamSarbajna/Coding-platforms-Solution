3c3f277fc76707660d333e59ebb49fd8368f9df5
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-01-03 12:15:08 +0530

    Create RECTANGL.cpp

---
+++ Code Chef/RECTANGL/RECTANGL.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        if (a == b && c == d)
+            cout << "YES" << endl;
+        else if (a == c && b == d)
+            cout << "YES" << endl;
+        else if (a == d && b == c)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+            
+    }
+    return 0;
+}
