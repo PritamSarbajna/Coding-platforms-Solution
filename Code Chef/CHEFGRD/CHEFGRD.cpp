a91cf474a412140a480225f64cb89b145c5d2006
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-01-05 21:43:56 +0530

    Create CHEFGRD.cpp

---
+++ Code Chef/CHEFGRD/CHEFGRD.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t; 
+	cin >> t;
+	while(t--){
+	    int n, i, j;
+	    cin >> n >> i >> j;
+	    int ans = (i+j)%2;
+	    cout << ans << endl;
+	}
+	return 0;
+}
