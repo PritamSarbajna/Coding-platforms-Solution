657be611911d31e69f9e633c12d9eb25e5e9c730
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-04-26 21:35:06 +0530

    Create Contains_Duplicate.cpp

---
+++ Leet Code/Contains Duplicate/Contains_Duplicate.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> nums_set(nums.begin(), nums.end());
+        return nums_set.size() != nums.size();
+    }
+};
