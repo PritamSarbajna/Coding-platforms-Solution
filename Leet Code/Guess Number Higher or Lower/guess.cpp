b1df513d6558a718d464e88cba27abaa00cfb942
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-06-01 12:18:33 +0530

    Create guess.cpp

---
+++ Leet Code/Guess Number Higher or Lower/guess.cpp
@@ -0,0 +1,31 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        long int left = 1;
+        long int right = n;
+        
+        while(right >= left){
+            long int mid = (right+left)/2;
+            
+            if(guess(mid)==0){
+                return mid;
+            }
+            else if(guess(mid)==-1){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+};
