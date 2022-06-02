bbe21b8a33aabd795415d8e4788820bced3cb30c
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-06-02 21:56:07 +0530

    Update move_zeroes.cpp

--- Leet Code/Move Zeroes/move_zeroes.cpp
+++ Leet Code/Move Zeroes/move_zeroes.cpp
@@ -1,19 +1,37 @@
+// // Brute force solution
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         for (int i = 0; i < nums.size(); i++){            
+//             if (nums[i] != 0){
+//                 continue;
+//             } 
+//             else {
+//                 for (int j = i + 1; j < nums.size(); j++){
+//                     if (nums[j] != 0){
+//                         std::swap(nums[i], nums[j]);
+//                         break;
+//                     }
+//                     else
+//                         continue;
+//                 }
+//             }
+//         }
+//     }
+// };
+
+// Two pointer solution
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++){            
-            if (nums[i] != 0){
-                continue;
-            } 
-            else {
-                for (int j = i + 1; j < nums.size(); j++){
-                    if (nums[j] != 0){
-                        std::swap(nums[i], nums[j]);
-                        break;
-                    }
-                    else
-                        continue;
-                }
+        int n = nums.size();
+        int i = 0, j = 0;
+        while(j<n){
+            if(nums[j]==0) j++;
+            else{
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
             }
         }
     }
