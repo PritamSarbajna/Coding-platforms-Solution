d4eaa52d8ec6a5293e5fa2b7895b4511af734c12
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-06-05 12:10:18 +0530

    Create soldier_bananas.cpp

---
+++ Codeforces/Soldier and Bananas/soldier_bananas.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int k, n, w;
+    cin >> k >> n >> w;
+
+    int total = 0;
+    for(int i=1; i<=w; i++){
+        total += (i*k);
+    }
+    
+    if(total <= n){
+        cout << 0 << endl;
+    }
+    else{
+        cout << total-n << endl;
+    }
+    return 0;
+}
