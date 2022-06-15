07779477a3ce11696a57d03adf0261f4a524d0d6
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-06-15 21:27:27 +0530

    Create BURGERS.cpp

---
+++ Code Chef/BURGERS/BURGERS.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int a, b;
+	    cin >> a >> b;
+	    int count = 0;
+	    
+	    if(a>=b){
+	        cout << b << endl;
+	    }
+	    else{
+	        cout << a << endl;
+	    }
+	}
+	return 0;
+}
