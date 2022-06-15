4f3f869501ca18ee279288032e68577599773e6b
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-06-15 21:26:17 +0530

    Create WAITTIME.cpp

---
+++ Code Chef/WAITTIME/WAITTIME.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int k, x;
+	    cin >> k >> x;
+	    cout << (k*7) - x << endl;
+	}
+	return 0;
+}
