e497205ab76b6cf906c99f91009424e8cc0a57d2
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-06-17 20:36:28 +0530

    Create count_odd_even.cpp

---
+++ GeeksForGeeks/Count Odd Even/count_odd_even.cpp
@@ -0,0 +1,23 @@
+class Solution{
+    public:
+    /*Function to count even and odd elements in the array
+    * arr : Array with its elements
+    * sizeof_array : number of array elements
+    * countOdd : variable to count number of odd elements
+    * countEven : variable to count number of even elements
+    */
+    void countOddEven(int arr[], int sizeof_array)
+    {
+        
+        // your code here
+       int countOdd=0, countEven=0;
+       for(int i=0;i<sizeof_array;i++){
+           if(arr[i]%2==0)
+               countEven++;
+           else
+               countOdd++;
+       }
+      cout<<countOdd<<" "<<countEven<<endl;
+        
+    }
+};
