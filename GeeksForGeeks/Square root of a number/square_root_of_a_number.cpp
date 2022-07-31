2ee1720bcae16f41d256354197467c90fa6ec704
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-07-31 16:34:53 +0530

    Create square_root_of_a_number.cpp

---
+++ GeeksForGeeks/Square root of a number/square_root_of_a_number.cpp
@@ -0,0 +1,8 @@
+// Function to find square root
+// x: element to find square root
+class Solution{
+  public:
+    long long int floorSqrt(long long int x){
+        return floor(sqrt(x));
+    }
+};
