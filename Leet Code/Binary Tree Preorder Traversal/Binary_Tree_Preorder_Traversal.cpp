02e3ecaa1003a7127af6c6c37dcc65b48b2545d8
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-08-31 21:22:38 +0530

    Create Binary_Tree_Preorder_Traversal.cpp

---
+++ Leet Code/Binary Tree Preorder Traversal/Binary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> tree;
+        
+        if(root == NULL){
+            return tree;
+        }
+        
+        stack<TreeNode*> stack;
+        stack.push(root);
+        
+        while(!stack.empty()){
+            root = stack.top();
+            stack.pop();
+            
+            tree.push_back(root->val);
+            if(root->right){
+                stack.push(root->right);
+            }
+            
+            if(root->left){
+                stack.push(root->left);
+            }
+        }
+        
+        return tree;
+    }
+};
