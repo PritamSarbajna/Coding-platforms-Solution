114a1ed1f1012ec51910360ad66c4d82533738eb
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-09-08 21:20:57 +0530

    Update sort_linked_list_0_1_2.cpp

--- GeeksForGeeks/Given a linked list of 0s, 1s and 2s, sort it/sort_linked_list_0_1_2.cpp
+++ GeeksForGeeks/Given a linked list of 0s, 1s and 2s, sort it/sort_linked_list_0_1_2.cpp
@@ -41,3 +41,59 @@ class Solution
         
     }
 };
+
+
+
+/* Approach 2 */
+class Solution
+{
+    public:
+    
+    void insertAtTail(Node* &Tail, Node *temp){
+        Tail->next = temp;
+        Tail = temp;
+    }
+    
+    //Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node *head) {
+        Node* zeroHead = new Node(-1);
+        Node* zeroTail = zeroHead;
+        Node* oneHead = new Node(-1);
+        Node* oneTail = oneHead;
+        Node* twoHead = new Node(-1);
+        Node* twoTail = twoHead;
+        
+        Node* temp = head;
+        
+        while(temp != NULL){
+            if(temp->data == 0){
+                insertAtTail(zeroTail, temp);
+            }
+            if(temp->data == 1){
+                insertAtTail(oneTail, temp);
+            }
+            if(temp->data == 2){
+                insertAtTail(twoTail, temp);
+            }
+            temp = temp->next;
+        }
+        
+        if(oneHead->next != NULL){
+            zeroTail->next = oneHead->next;
+        }
+        else {
+            zeroTail->next = twoHead->next;
+        }
+        
+        oneTail->next = twoHead->next;
+        twoTail->next = NULL;
+        
+        head = zeroHead->next;
+        
+        delete zeroHead;
+        delete oneHead;
+        delete twoHead;
+        
+        return head;
+    }
+};
