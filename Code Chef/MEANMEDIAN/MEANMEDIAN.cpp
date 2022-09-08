6be883831641e292c131740726427a305b63483f
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-09-08 20:11:00 +0530

    Create MEANMEDIAN.cpp

---
+++ Code Chef/MEANMEDIAN/MEANMEDIAN.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+int main() {
+	ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int t;
+    cin >> t;
+    while(t--){
+        vi ans;
+        int x, y;
+        cin >> x >> y;
+        
+        cout << y << " " << y << " " << (3*x)-(2*y) << endl;
+    }
+}
