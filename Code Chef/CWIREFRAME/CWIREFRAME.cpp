610c2ded28c1065365c0f529fa6c2fd804ea189f
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-09-10 17:20:38 +0530

    Create CWIREFRAME.cpp

---
+++ Code Chef/CWIREFRAME/CWIREFRAME.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m, x;
+        cin >> n >> m >> x;
+        
+        
+        cout << (2*(n+m))*x << endl;
+    }
+}
