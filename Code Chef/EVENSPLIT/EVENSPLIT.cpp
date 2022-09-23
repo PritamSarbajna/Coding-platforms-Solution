826c877444d200ceccf775ee6392cc5e1fa536e7
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2022-09-23 21:25:47 +0530

    Create  EVENSPLIT.cpp
    ```
    /*
    
    
     _____      _ _                     _____            _           _            
     |  __ \    (_) |                   / ____|          | |         (_)    
     | |__) | __ _| |_ __ _ _ __ ___   | (___   __ _ _ __| |__   __ _ _ _ __   __ _
     |  ___/ '__| | __/ _` | '_ ` _ \   \___ \ / _` | '__| '_ \ / _` | | '_ \ / _` |
     | |   | |  | | || (_| | | | | | |  ____) | (_| | |  | |_) | (_| | | | | | (_| |
     |_|   |_|  |_|\__\__,_|_| |_| |_| |_____/ \__,_|_|  |_.__/ \__,_| |_| |_|\__,_|
                                                                    _/ |
                                                                   |__/
    
    
    */
    
    
    
    #include <bits/stdc++.h>
    using namespace std;
    
    // For ordered set
    #include <ext/pb_ds/assoc_container.hpp>
    #include <ext/pb_ds/tree_policy.hpp>
    using namespace __gnu_pbds;
    template <class T>
    using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
    
    #define ar array
    #define ll long long
    #define vi vector<int>
    #define vll vector<long long>
    #define endl "\n"
    #define vi vector<int>
    #define pb push_back
    #define mii map<int, int>
    
    int main() {
        ios_base::sync_with_stdio(0);
        cin.tie(0); cout.tie(0);
    
        int t;
        cin >> t;
        while(t--){
            int n; cin >> n;
            string str; cin >> str;
            
            // If string length is less than 3 print as it is
            if(n < 3){
                cout << str << endl;
                continue;
            }
            
            sort(str.begin(), str.end());
            cout << str << endl;
        }
    
        return 0;
    }

---
+++ Code Chef/EVENSPLIT/EVENSPLIT.cpp
@@ -0,0 +1,58 @@
+/*
+
+
+ _____      _ _                     _____            _           _            
+ |  __ \    (_) |                   / ____|          | |         (_)    
+ | |__) | __ _| |_ __ _ _ __ ___   | (___   __ _ _ __| |__   __ _ _ _ __   __ _
+ |  ___/ '__| | __/ _` | '_ ` _ \   \___ \ / _` | '__| '_ \ / _` | | '_ \ / _` |
+ | |   | |  | | || (_| | | | | | |  ____) | (_| | |  | |_) | (_| | | | | | (_| |
+ |_|   |_|  |_|\__\__,_|_| |_| |_| |_____/ \__,_|_|  |_.__/ \__,_| |_| |_|\__,_|
+                                                                _/ |
+                                                               |__/
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// For ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define ar array
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+#define vi vector<int>
+#define pb push_back
+#define mii map<int, int>
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n; cin >> n;
+        string str; cin >> str;
+        
+        // If string length is less than 3 print as it is
+        if(n < 3){
+            cout << str << endl;
+            continue;
+        }
+        
+        sort(str.begin(), str.end());
+        cout << str << endl;
+    }
+
+    return 0;
+}
