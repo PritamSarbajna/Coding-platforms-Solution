41553d2e13127fbf034da306b2f2bbfe360985b4
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-03-09 12:10:14 +0530

    Update Linked_List_Cycle_II.cpp
    ```
    /**
     * Definition for singly-linked list.
     * struct ListNode {
     *     int val;
     *     ListNode *next;
     *     ListNode(int x) : val(x), next(NULL) {}
     * };
     */
    class Solution {
    public:
        ListNode *detectCycle(ListNode *head) {
    
            if(head == NULL){
                return NULL;
            }
    
            ListNode *low = head, *high = head;
            bool cycle = false;
            
            while(high != NULL && high->next != NULL){
    
                low = low->next;
                high = high->next->next;
    
                if(low == high){
                    cycle = true;
                    break;
                }
            }
            
            if(!cycle){
                return NULL;
            }
            else{
                high = head;
                while(low != high){
                    low = low->next;
                    high = high->next;
                }
                return low;
            }
        }
    };

--- Leet Code/Linked List Cycle II/Linked_List_Cycle_II.cpp
+++ Leet Code/Linked List Cycle II/Linked_List_Cycle_II.cpp
@@ -1,3 +1,5 @@
+// Approach 1 :
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -34,5 +36,53 @@ public:
             last = last->next;
         }
         return start;
+    }
+};
+
+
+
+// Approach 2 :
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        if(head == NULL){
+            return NULL;
+        }
+
+        ListNode *low = head, *high = head;
+        bool cycle = false;
+        
+        while(high != NULL && high->next != NULL){
+
+            low = low->next;
+            high = high->next->next;
+
+            if(low == high){
+                cycle = true;
+                break;
+            }
+        }
+        
+        if(!cycle){
+            return NULL;
+        }
+        else{
+            high = head;
+            while(low != high){
+                low = low->next;
+                high = high->next;
+            }
+            return low;
+        }
     }
 };
