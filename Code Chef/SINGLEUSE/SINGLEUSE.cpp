9a070bcf04cbf1d983a70042e311aac8af5ee8c1
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-03-09 17:32:19 +0530

    Create SINGLEUSE.cpp

---
+++ Code Chef/SINGLEUSE/SINGLEUSE.cpp
@@ -0,0 +1,58 @@
+/*
+
+
+ _____      _ _                     _____            _           _            
+ |  __ \    (_) |                   / ____|          | |         (_)    
+ | |__) | __ _| |_ __ _ _ __ ___   | (___   __ _ _ __| |__   __ _ _ _ __   __ _
+ |  ___/ '__| | __/ _` | '_ ` _ \   \___ \ / _` | '__| '_ \ / _` | | '_ \ / _` |
+ | |   | |  | | || (_| | | | | | |  ____) | (_| | |  | |_) | (_| | | | | | (_| |
+ |_|   |_|  |_|\__\__,_|_| |_| |_| |_____/ \__,_|_|  |_.__/ \__,_| |_| |_|\__,_|
+                                                                _/ |
+                                                               |__/
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// For ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define ar array
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+#define vi vector<int>
+#define pb push_back
+#define mii map<int, int>
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int h, x, y; cin >> h >> x >> y;
+        int count = 0;
+        
+        h -= y;
+        count++;
+        
+        while(h > 0){
+            h -= x;
+            count++;
+        }
+        cout << count << endl;
+    }
+
+    return 0;
+}
