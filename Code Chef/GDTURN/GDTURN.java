7e36f60d9ea6fb63898ac12c03bc6e47137bbb0b
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-03-13 23:52:36 +0530

    Create
    ```
    /* package codechef; // don't place package name! */
    
    import java.util.*;
    import java.lang.*;
    import java.io.*;
    
    /* Name of the class has to be "Main" only if the class is public. */
    class Codechef
    {
    	public static void main (String[] args) throws java.lang.Exception
    	{
    		Scanner sc = new Scanner(System.in);
    		int t = sc.nextInt();
    		while(t-->0){
    		    int x = sc.nextInt();
    		    int y = sc.nextInt();
    		    
    		    System.out.println((x+y)>6? "YES":"NO");
    		}
    	}
    }

---
+++ Code Chef/GDTURN/GDTURN.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    
+		    System.out.println((x+y)>6? "YES":"NO");
+		}
+	}
+}
