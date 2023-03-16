4f139647fc18fefbf6a165c30b85cc8761bd1d2b
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-03-16 22:55:59 +0530

    Create

---
+++ Leet Code/Partitioning Into Minimum Number Of Deci-Binary Numbers/answer.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int value(char num){
+        if(num == '0'){
+            return 1;
+        }
+        else{
+            return num - '0';
+        }
+    }
+    public int minPartitions(String n) {
+        int sum = 0;
+
+        for(int i=0; i<n.length(); i++){
+            int val = value(n.charAt(i)) - sum;
+
+            if(val < 0){
+                sum += 0;
+            }
+            else{
+                sum += val;
+            }
+        }
+        return sum;
+    }
+}
