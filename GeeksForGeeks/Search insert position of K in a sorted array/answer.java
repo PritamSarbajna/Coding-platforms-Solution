3169c101bce4c6a227287858861164acc5308eb2
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-03-19 17:37:42 +0530

    Create

---
+++ GeeksForGeeks/Search insert position of K in a sorted array/answer.java
@@ -0,0 +1,22 @@
+class Solution
+{
+    static int searchInsertK(int Arr[], int N, int k)
+    {
+        int low = 0, high = N-1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(Arr[mid] == k){
+                return mid;
+            }
+            else if(Arr[mid] < k){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+}
