da9e2cbc92a4046b759ae7386ab211c8b4cb9ca8
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-04-18 13:38:30 +0530

    Create answer.cpp

---
+++ Leet Code/Merge Strings Alternately/answer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans;
+
+        // Adding characters alternately 
+        int i=0; // i required for future so declared outside of loop
+        for(; i<word1.size() && i<word2.size(); i++){
+            ans.push_back(word1[i]);
+            ans.push_back(word2[i]);
+        }
+
+        // Appending the last part of greater length string
+        if(i == word1.size()){
+            for(int j=i; j<word2.size(); j++){
+                ans.push_back(word2[j]);
+            }
+        }
+        else if(i == word2.size()){
+            for(int j=i; j<word1.size(); j++){
+                ans.push_back(word1[j]);
+            }
+        }
+
+        return ans;
+    }
+};
