ca9f57f8c61ccc1bf93eb96ca17af4f5685f2142
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-04-22 12:57:26 +0530

    Create answer.cpp

---
+++ GeeksForGeeks/Number of 1 Bits/answer.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int setBits(int n) {
+       int count = 0;
+       while(n){
+           count += (n&1);
+           n = n >> 1;
+       }
+       return count;
+    }
+};
