acfc5cb714e9fe35785d1d2019ed4b0dcf6c10b5
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-04-23 23:09:14 +0530

    Create HQ9+.cpp

---
+++ Codeforces/HQ9+/HQ9+.cpp
@@ -0,0 +1,54 @@
+/*
+
+
+ _____      _ _                     _____            _           _            
+ |  __ \    (_) |                   / ____|          | |         (_)    
+ | |__) | __ _| |_ __ _ _ __ ___   | (___   __ _ _ __| |__   __ _ _ _ __   __ _
+ |  ___/ '__| | __/ _` | '_ ` _ \   \___ \ / _` | '__| '_ \ / _` | | '_ \ / _` |
+ | |   | |  | | || (_| | | | | | |  ____) | (_| | |  | |_) | (_| | | | | | (_| |
+ |_|   |_|  |_|\__\__,_|_| |_| |_| |_____/ \__,_|_|  |_.__/ \__,_| |_| |_|\__,_|
+                                                                _/ |
+                                                               |__/
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// For ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define ar array
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+#define vi vector<int>
+#define pb push_back
+#define mii map<int, int>
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string s; cin >> s;
+    bool flag = false;
+
+    for(int i=0; i<s.length(); i++){
+        if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9'){
+            flag = true;
+        }
+    }
+
+    cout << ((flag)? "YES" : "NO") << endl;
+    
+
+    return 0;
+}
