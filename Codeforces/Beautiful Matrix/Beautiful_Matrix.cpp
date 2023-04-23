7c52b71a0a66b08370b9cf8ad13800621dd1a2f2
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-04-23 20:49:35 +0530

    Create Beautiful_Matrix.cpp

---
+++ Codeforces/Beautiful Matrix/Beautiful_Matrix.cpp
@@ -0,0 +1,60 @@
+/*
+
+
+ _____      _ _                     _____            _           _            
+ |  __ \    (_) |                   / ____|          | |         (_)    
+ | |__) | __ _| |_ __ _ _ __ ___   | (___   __ _ _ __| |__   __ _ _ _ __   __ _
+ |  ___/ '__| | __/ _` | '_ ` _ \   \___ \ / _` | '__| '_ \ / _` | | '_ \ / _` |
+ | |   | |  | | || (_| | | | | | |  ____) | (_| | |  | |_) | (_| | | | | | (_| |
+ |_|   |_|  |_|\__\__,_|_| |_| |_| |_____/ \__,_|_|  |_.__/ \__,_| |_| |_|\__,_|
+                                                                _/ |
+                                                               |__/
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// For ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define ar array
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+#define vi vector<int>
+#define pb push_back
+#define mii map<int, int>
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int matrix[5][5];
+
+    // for storing purpose
+    int one_pos_i, one_pos_j;
+
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            cin >> matrix[i][j];
+
+            if(matrix[i][j] == 1){
+                one_pos_i = i+1; one_pos_j = j+1;
+            }
+        }
+    }
+
+    cout << abs(one_pos_i - 3) + abs(one_pos_j - 3) << endl;
+
+
+    return 0;
+}
