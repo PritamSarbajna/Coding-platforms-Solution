dd2ba8f23db08977499cd826f7516e1e849222c9
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-04-24 18:59:29 +0530

    Create Ilya_and_Bank_Account.cpp

---
+++ Codeforces/Ilya and Bank Account/Ilya_and_Bank_Account.cpp
@@ -0,0 +1,57 @@
+/*
+
+
+ _____      _ _                     _____            _           _            
+ |  __ \    (_) |                   / ____|          | |         (_)    
+ | |__) | __ _| |_ __ _ _ __ ___   | (___   __ _ _ __| |__   __ _ _ _ __   __ _
+ |  ___/ '__| | __/ _` | '_ ` _ \   \___ \ / _` | '__| '_ \ / _` | | '_ \ / _` |
+ | |   | |  | | || (_| | | | | | |  ____) | (_| | |  | |_) | (_| | | | | | (_| |
+ |_|   |_|  |_|\__\__,_|_| |_| |_| |_____/ \__,_|_|  |_.__/ \__,_| |_| |_|\__,_|
+                                                                _/ |
+                                                               |__/
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// For ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define ar array
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+#define vi vector<int>
+#define pb push_back
+#define mii map<int, int>
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n; cin >> n;
+
+    if(n >= 0){
+        cout << n << endl;
+        return 0;
+    }
+
+    int num1 = n/10;
+    int x = n%10;
+    int num2 = (n/100)*10 + x;
+
+    int max_num = max(num1, num2);
+
+    cout << max(n, max_num) << endl;
+
+    return 0;
+}
