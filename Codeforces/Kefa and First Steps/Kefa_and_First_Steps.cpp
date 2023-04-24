731c89a3ae9481e7285b82410902bc8c3444ef2c
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-04-24 17:36:26 +0530

    Create Kefa_and_First_Steps.cpp

---
+++ Codeforces/Kefa and First Steps/Kefa_and_First_Steps.cpp
@@ -0,0 +1,72 @@
+/*
+
+
+ _____      _ _                     _____            _           _            
+ |  __ \    (_) |                   / ____|          | |         (_)    
+ | |__) | __ _| |_ __ _ _ __ ___   | (___   __ _ _ __| |__   __ _ _ _ __   __ _
+ |  ___/ '__| | __/ _` | '_ ` _ \   \___ \ / _` | '__| '_ \ / _` | | '_ \ / _` |
+ | |   | |  | | || (_| | | | | | |  ____) | (_| | |  | |_) | (_| | | | | | (_| |
+ |_|   |_|  |_|\__\__,_|_| |_| |_| |_____/ \__,_|_|  |_.__/ \__,_| |_| |_|\__,_|
+                                                                _/ |
+                                                               |__/
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// For ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define ar array
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+#define vi vector<int>
+#define pb push_back
+#define mii map<int, int>
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n; cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int max_count = INT_MIN;
+    int count = 1;
+
+    // If array has 1 element
+    if(n == 1){
+        cout << count << endl;
+        return 0;
+    }
+
+    for(int i=0; i<n-1; i++){
+        if(arr[i] <= arr[i+1]){
+            count++;
+        }
+        else{
+            count = 1;
+        }
+
+        max_count = max(max_count, count);
+
+    }
+
+
+    cout << max_count << endl;
+
+    return 0;
+}
