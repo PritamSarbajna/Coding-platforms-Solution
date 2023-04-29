4f298673fe9135c64ff8eed43b195f31ca689361
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-04-29 22:06:54 +0530

    Create Dubstep.cpp

---
+++ Codeforces/Dubstep/Dubstep.cpp
@@ -0,0 +1,63 @@
+/*
+
+
+ _____      _ _                     _____            _           _            
+ |  __ \    (_) |                   / ____|          | |         (_)    
+ | |__) | __ _| |_ __ _ _ __ ___   | (___   __ _ _ __| |__   __ _ _ _ __   __ _
+ |  ___/ '__| | __/ _` | '_ ` _ \   \___ \ / _` | '__| '_ \ / _` | | '_ \ / _` |
+ | |   | |  | | || (_| | | | | | |  ____) | (_| | |  | |_) | (_| | | | | | (_| |
+ |_|   |_|  |_|\__\__,_|_| |_| |_| |_____/ \__,_|_|  |_.__/ \__,_| |_| |_|\__,_|
+                                                                _/ |
+                                                               |__/
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// For ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define ar array
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+#define vi vector<int>
+#define pb push_back
+#define mii map<int, int>
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string s; cin >> s;
+    int i = 0;
+    string ans;
+    bool prev = false;
+
+    while(i < s.length()){
+        if(s[i] == 'W' && s[i+1] == 'U' && s[i+2] == 'B'){
+            if(ans.length() > 0 && prev == false){
+                ans.push_back(' ');
+                prev = true;
+            }
+            i += 3;
+        }
+        else{
+            ans.push_back(s[i]);
+            i++;
+            prev = false;
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
