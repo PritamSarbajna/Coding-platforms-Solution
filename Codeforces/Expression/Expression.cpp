9f33cdbb3a460bccace44b91edddb685a3c68ce0
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-04-30 12:49:32 +0530

    Create Expression.cpp

---
+++ Codeforces/Expression/Expression.cpp
@@ -0,0 +1,52 @@
+/*
+
+
+ _____      _ _                     _____            _           _            
+ |  __ \    (_) |                   / ____|          | |         (_)    
+ | |__) | __ _| |_ __ _ _ __ ___   | (___   __ _ _ __| |__   __ _ _ _ __   __ _
+ |  ___/ '__| | __/ _` | '_ ` _ \   \___ \ / _` | '__| '_ \ / _` | | '_ \ / _` |
+ | |   | |  | | || (_| | | | | | |  ____) | (_| | |  | |_) | (_| | | | | | (_| |
+ |_|   |_|  |_|\__\__,_|_| |_| |_| |_____/ \__,_|_|  |_.__/ \__,_| |_| |_|\__,_|
+                                                                _/ |
+                                                               |__/
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// For ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+#define vi vector<int>
+#define pb push_back
+#define mii map<int, int>
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int u = a+b+c;
+    int w = (a+b) * c;
+    int x = a + (b*c);
+    int y = a* (b+c);
+    int z = a*b*c;
+
+    cout << max(max(max(w,x), max(y,z)), u) << endl;
+
+    return 0;
+}
