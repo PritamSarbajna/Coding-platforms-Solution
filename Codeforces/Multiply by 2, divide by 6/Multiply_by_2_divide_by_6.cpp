fa04e5f48f892e32475a74473cd19d3134724b75
Pritam Sarbajna <90236635+PritamSarbajna@users.noreply.github.com>
2023-05-15 19:40:48 +0530

    Create Multiply_by_2_divide_by_6.cpp

---
+++ Codeforces/Multiply by 2, divide by 6/Multiply_by_2_divide_by_6.cpp
@@ -0,0 +1,65 @@
+/*
+
+
+ _____      _ _                     _____            _           _            
+ |  __ \    (_) |                   / ____|          | |         (_)    
+ | |__) | __ _| |_ __ _ _ __ ___   | (___   __ _ _ __| |__   __ _ _ _ __   __ _
+ |  ___/ '__| | __/ _` | '_ ` _ \   \___ \ / _` | '__| '_ \ / _` | | '_ \ / _` |
+ | |   | |  | | || (_| | | | | | |  ____) | (_| | |  | |_) | (_| | | | | | (_| |
+ |_|   |_|  |_|\__\__,_|_| |_| |_| |_____/ \__,_|_|  |_.__/ \__,_| |_| |_|\__,_|
+                                                                _/ |
+                                                               |__/
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// For ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define ar array
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+#define vi vector<int>
+#define pb push_back
+#define mii map<int, int>
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+
+        int count = 0;
+
+        while(n != 1){
+            if(n%6 == 0){
+                n /= 6;
+            }
+            else if(n%3 == 0){
+                n *= 2;
+            }
+            else{
+                count = -1;
+                break;
+            }
+            count++;
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}
